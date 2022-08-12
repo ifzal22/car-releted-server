fce6a2c3ee83651be659f3aa853fd3d1eeaa1000
ifzal22 <enjoyyourlife2244@gmailm>
2022-08-13 02:38:13 +0600

    make it better

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const ObjectId = require('mongodb').ObjectId;
 const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+// const fileUpload = require('express-fileupload');
+
+
+// const fileUpload = require('express')
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqdph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -33,7 +38,7 @@ const port =  process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+// app.use(fileUpload());
 async function verifyToken(req, res, next) {
     if (req.headers?.authorization?.startsWith('Bearer ')) {
         const token = req.headers.authorization.split(' ')[1];
@@ -63,6 +68,8 @@ async function run(){
     const reviewCollection = database.collection('review');
     const servicesCollection2 = database.collection('services');
 
+    const carUpoadImage  = database.collection('carUpoadImage')
+
     // GET API
 
     app.get("/AllCars", async(req,res) =>{
@@ -84,15 +91,24 @@ app.get("/services", async(req,res) =>{
     res.send(services);
   })
 
+
+
+
       app.post('/addCars', async(req, res)=>{
         const service = req.body;
     //    console.log('hit the post api', service);
-      
-      
-      const result = await servicesCollection.insertOne(service)
+      const pic = req.body.image
+      picData= pic.data;
+      const encodedPic= picData.toString('base64')
+      const imageBuffer = Buffer.from(encodedPic, 'base64')
+
+
+      const result = await servicesCollection.insertOne(service,imageBuffer)
       console.log(result);
-      res.json(result)
+      res.json(result,imageBuffer)
       })
+
+
 
       // get single package
       app.get("/AllCars/:id", async (req, res) => {
